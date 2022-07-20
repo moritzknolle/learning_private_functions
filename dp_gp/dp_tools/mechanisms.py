a39b27ce5b362e610b829b03a056b1c98edfa88f
Moritz <moritz.knolle@me.com>
2022-07-20 19:19:16 +0200

    refactoring and fixes

--- dp_gp/dp_tools/mechanisms.py
+++ dp_gp/dp_tools/mechanisms.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def laplace_mechanism(inp_arr: np.ndarray, eps: float, delta:float, sens: float):
+def laplace_mechanism(inp_arr: np.ndarray, eps: float, delta: float, sens: float):
     """Applies the LaPlace mechanism to achieve (eps, 0) differential privacy for inp_arr
     Args:
         inp_arr (np.ndarray): input array to be privatised
@@ -17,15 +17,18 @@ def laplace_mechanism(inp_arr: np.ndarray, eps: float, delta:float, sens: float)
     return priv_out
 
 
+# TODO adapt to analytic Gaussian mechanism
 def gauss_mechanism(inp_arr: np.ndarray, eps: float, delta: float, sens: float):
-    """Applies the Gaussian mechanism to achieve (eps, delta) differential privacy for inp_arr
+    """Applies the Gaussian mechanism (Balle and Wang, 2018) to achieve (eps, delta) differential privacy for inp_arr
     Args:
         inp_arr (np.ndarray): input array to be privatised
         eps: privacy budget epsilon
         delta: failure probability by which the guarantee doesn't hold exactly
         sens: global sensitivity, i.e. the maximal bound by which the contents of inp_arr can change
     """
-    print(f"Adding independent Gaussian noise with std={sens * (np.sqrt(2 * np.log(1.25 / delta))) / eps:4f}")
+    print(
+        f"Adding independent Gaussian noise with std={sens * (np.sqrt(2 * np.log(1.25 / delta))) / eps:4f}"
+    )
     noise_sample = np.random.normal(
         loc=0,
         scale=sens * (np.sqrt(2 * np.log(1.25 / delta))) / eps,
@@ -33,16 +36,3 @@ def gauss_mechanism(inp_arr: np.ndarray, eps: float, delta: float, sens: float):
     )
     priv_out = inp_arr + noise_sample
     return priv_out
-
-
-def analytic_gauss_mechanism(
-    inp_arr: np.ndarray, eps: float, delta: float, sens: float
-):
-    """Applies the analytic Gaussian mechanism (Balle and Wang, 2018) to achieve (eps, delta) differential privacy for inp_arr
-    Args:
-        inp_arr (np.ndarray): input array to be privatised
-        eps: privacy budget epsilon
-        delta: failure probability by which the guarantee doesn't hold exactly
-        sens: global sensitivity, i.e. the maximal bound by which the contents of inp_arr can change
-    """
-    raise NotImplementedError()
