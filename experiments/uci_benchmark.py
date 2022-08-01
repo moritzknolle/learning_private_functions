4047249983cfdfdfe086e76dbd580344e10cefbb
Moritz <moritz.knolle@me.com>
2022-08-01 09:42:37 +0200

    refactor + experiment setup uci

--- experiments/uci_benchmark.py
+++ experiments/uci_benchmark.py
@@ -1,59 +1,121 @@
-import os, sys, inspect, random, time
+import os, sys, inspect, random, time, gc
 from uci_datasets import all_datasets, Dataset
 import tensorflow as tf
 import numpy as np
+import gpflow
 import pandas as pd
+from absl import flags, app
+import matplotlib.pyplot as plt
+from gpflow.utilities import print_summary
 
 # add parent directory to sys.path
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 sys.path.insert(0, os.path.dirname(currentdir))
 
 from utils import make_deterministic
+from dp_gp.dp_tools.mechanisms import gauss_mechanism, laplace_mechanism
 from dp_gp.approximate_inference.common_train_logic import (
     make_SVGP_model,
     simple_training_loop,
 )
 
+# Hyperparameters
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("batch_size", 100, "batch size for training")
+flags.DEFINE_integer("num_inducing", 512, "Number of inducing variables")
+flags.DEFINE_float("lr", 0.01, "learning rate")
+flags.DEFINE_integer("epochs", 50, "number of epochs")
+flags.DEFINE_bool("save_results", True, "whether to dump results to a .csv file")
+flags.DEFINE_integer("max_observations", 300, "max number of observations per dataset. Used to select suitable datasets from UCI repository")
+flags.DEFINE_integer("n_folds", 3, "number of folds to perform for cross-validation")
+flags.DEFINE_float("epsilon", 1.0, "privacy budget")
+flags.DEFINE_float("delta", 0.01, "failure probability of privacy guarantee (for Gaussian mechanism)")
+
 # for reproducible experiments
-make_deterministic()
+make_deterministic(seed=42)
 f64 = lambda x: np.array(x).astype(np.float64)
 
-results = {"Dataset": [], "MSE (test)": [], "NLP (test)":[], "run-time (s)": []}
-n_partitions = 3
-datasets = [
-    name
-    for name, (n_observations, n_dimensions) in all_datasets.items()
-    if n_observations < 1000
-]
-print("Performing benchmark on Datasets:", datasets)
-mse = tf.keras.losses.MeanSquaredError()
+def main(argv):
+    results = {
+        "Dataset": [],
+        "Model":[],
+        "epsilon": [],
+        "RMSE (test)": [],
+        "NLL (test)": [],
+        "run-time (s)": [],
+    }
+    # datasets = [
+    #      name
+    #      for name, (n_observations, n_dimensions) in all_datasets.items()
+    #      if n_observations > FLAGS.max_observations
+    # ]
+    datasets = ['concrete']
+    print("Performing benchmark on Datasets:", datasets)
+    mse = tf.keras.losses.MeanSquaredError()
 
-for dataset_name in datasets:
-    data = Dataset(dataset_name)
-    start_time = time.time()
-    mse_vals = []
-    nlp_vals = []
-    for n in range(n_partitions):
-        x_train, y_train, x_test, y_test = data.get_split(split=n)
-        N = x_train.shape[0]
-        model = make_SVGP_model(
-            num_inducing=100, num_data=N, num_features=x_train.shape[1]
-        )
-        optimizer = tf.keras.optimizers.Adam(lr=1e-3)
-        simple_training_loop(
-            model=model, data=(x_train, y_train), optimizer=optimizer, epochs=50, batch_size=128, apply_dp=False
-        )
-        y_pred = model.predict_f(f64(x_test))
-        test_mse = mse(y_test, y_pred).numpy()
-        nlp = -1 * model.predict_log_density((x_test, y_test)).numpy().mean()
-        mse_vals.append(test_mse)
-        nlp_vals.append(nlp)
-    results['Dataset'].append(dataset_name)
-    results['MSE (test)'].append(f"{np.mean(mse_vals):.4f} ± {np.std(mse_vals):.4f}")
-    results['NLP (test)'].append(f"{np.mean(nlp_vals):.4f} ± {np.std(nlp_vals):.4f}")
-    results['run-time (s)'].append((time.time() - start_time)/n_partitions)
+    for dataset_name in datasets:
+        data = Dataset(dataset_name)
+        for mode in ["SVGP", "Local-DP"]:
+            start_time = time.time()
+            nlp_vals = []
+            mse_vals = []
+            model = None
+            gc.collect()
+            for n in range(FLAGS.n_folds):
+                x_train, y_train, x_test, y_test = data.get_split(split=n)
+                if mode == "Local-DP":
+                    x_sens = np.max(x_train) - np.min(x_train)
+                    y_sens = np.max(y_train) - np.min(y_train)
+                    print(f"\n \n ...... sens x: {x_sens}, y:{y_sens}")
+                    x_train = laplace_mechanism(x_train, eps=FLAGS.epsilon, delta=0.0, sens=x_sens)
+                    y_train = laplace_mechanism(y_train, eps=FLAGS.epsilon, delta=0.0, sens=y_sens)
+                N = x_train.shape[0]
+                feature_dim = x_train.shape[1]
+                lengthscales=np.random.uniform(0.1, 5.0, size=(feature_dim))
+                kernel = gpflow.kernels.SquaredExponential(lengthscales=lengthscales)
+                model = make_SVGP_model(
+                    num_inducing=FLAGS.num_inducing, num_data=N, num_features=feature_dim, kernel=kernel
+                )
+                print_summary(model)
+                optimizer = tf.keras.optimizers.Adam(lr=1e-3)
+                simple_training_loop(
+                    model=model,
+                    data=(f64(x_train), f64(y_train)),
+                    optimizer=optimizer,
+                    epochs=FLAGS.epochs,
+                    batch_size=FLAGS.batch_size,
+                    apply_dp=False,
+                )
+                y_pred, _ = model.predict_f(f64(x_test))
+                y_pred = np.array(y_pred)
+                #print("y_test", y_test)
+                #print("\n y_pred", y_pred)
+                test_mse = mse(y_test, y_pred).numpy()
+                nlp = -1 * model.predict_log_density((f64(x_test), f64(y_test))).numpy().mean()
+                mse_vals.append(np.sqrt(test_mse))
+                nlp_vals.append(nlp)
+                plt.scatter(y_test, y_pred)
+                plt.xlabel("Prices")
+                plt.ylabel("Predicted prices")
+                plt.title("Prices vs Predicted prices")
+                plt.savefig(f"figures/price_preds_{n}.png")
+            if mode == "SVGP":
+                results['epsilon'].append(np.inf)
+            else:
+                results['epsilon'].append(FLAGS.epsilon)
+            results["Dataset"].append(dataset_name)
+            results["Model"].append(mode)
+            results["RMSE (test)"].append(
+                f"{np.mean(mse_vals):.3f} ± {np.std(mse_vals):.3f}"
+            )
+            results["NLL (test)"].append(
+                f"{np.mean(nlp_vals):.3f} ± {np.std(nlp_vals):.3f}"
+            )
+            results["run-time (s)"].append((time.time() - start_time) / FLAGS.n_folds)
 
+    df = pd.DataFrame.from_dict(results)
+    df.to_csv("experiments/uci_results.csv") if FLAGS.save_results else 0
+    print(df)
 
-df = pd.DataFrame.from_dict(results)
-df.to_csv("experiments/uci_results.csv")
-print(df)
+if __name__ == "__main__":
+    app.run(main)
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dtype = np.float64
 
 def make_deterministic(seed: int = 1234):
     """Makes PyTorch deterministic for reproducibility."""
@@ -9,3 +13,27 @@ def make_deterministic(seed: int = 1234):
     tf.keras.utils.set_random_seed(1)
     tf.config.experimental.enable_op_determinism()
 
+def plot_model(model, train_data, is_svgp:bool=True, title=""):
+    x, y = train_data
+    plt.figure(figsize=(12, 6))
+    ylim=(-2.2, 2.2)
+    plt.title(title) if title != "" else 0
+    pX = np.linspace(-1.1, 1.1, 100, dtype=dtype)[:, None]  # Test locations
+    pY, pYv = model.predict_y(pX)  # Predict Y values at test locations
+    plt.plot(x, y, ".", label="training points", alpha=0.2, markersize='10',color='#264185')
+    (line,) = plt.plot(pX, pY, lw=2.5, label="posterior mean", color='#A2D2FF', alpha=1.0)
+    plt.fill_between(
+        pX[:, 0],
+        (pY - 2 * pYv ** 0.5)[:, 0],
+        (pY + 2 * pYv ** 0.5)[:, 0],
+        color='#A2D2FF',
+        alpha=0.4,
+        lw=1.5,
+    )
+    if is_svgp:
+        Z = model.inducing_variable.Z.numpy()
+        plt.plot(Z, np.repeat(ylim[0], Z.shape[0]), 'k^', markersize='30', label="inducing locations", alpha=0.5, color='orange')
+    plt.legend(loc="lower right")
+    plt.ylim(ylim)
+    sns.despine()
+    plt.tight_layout()
