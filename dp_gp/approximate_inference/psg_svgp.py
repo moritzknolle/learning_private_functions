fixed per-sample ELBO calculation + tests

dp_gp/approximate_inference/psg_svgp.py
@@ -16,13 +16,15 @@ class SVGP_psg(gpflow.models.svgp.SVGP):
         f_mean, f_var = self.predict_f(X, full_cov=False, full_output_cov=False)
         var_exp = self.likelihood.variational_expectations(f_mean, f_var, Y)
         if self.num_data is not None:
-            num_data = tf.cast(self.num_data, kl.dtype)
-            minibatch_size = tf.cast(tf.shape(X)[0], kl.dtype)
-            scale = num_data / minibatch_size
+            scale = tf.cast(self.num_data, kl.dtype)
         else:
             scale = tf.cast(1.0, kl.dtype)
         if reduce:
+            minibatch_size = tf.cast(tf.shape(X)[0], kl.dtype) 
+            scale = scale / minibatch_size
             out = tf.reduce_sum(var_exp) * scale - kl
         else:
             out = var_exp * scale - kl
         return out
+    
+    

utils.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
 from sklearn.neighbors import KernelDensity
+from typing import Tuple
 
 dtype = np.float64
+
 
 def make_deterministic(seed: int = 1234):
     """Makes PyTorch deterministic for reproducibility."""
@@ -15,36 +17,70 @@ def make_deterministic(seed: int = 1234):
     tf.keras.utils.set_random_seed(1)
     tf.config.experimental.enable_op_determinism()
 
-def plot_model(model, train_data, test_data=None, is_svgp:bool=True, title=""):
+
+def plot_model(
+    model,
+    train_data,
+    test_data=None,
+    is_svgp: bool = True,
+    title="",
+    ms: str = "10",
+    predict_y: bool = True,
+    fig_size: Tuple = (10, 6),
+):
     x, y = train_data
-    plt.figure(figsize=(12, 6))
-    ylim=(-2.2, 2.2)
+    plt.figure(figsize=fig_size)
+    ylim = (-2.2, 2.2)
     plt.title(title) if title != "" else 0
     pX = np.linspace(-1.1, 1.1, 100, dtype=dtype)[:, None]  # Test locations
-    pY, pYv = model.predict_y(pX)  # Predict Y values at test locations
-    plt.plot(x, y, ".", label="training points", alpha=0.2, markersize='10',color='#264185')
+    pY, pYv = (
+        model.predict_y(pX) if predict_y else model.predict_f(pX)
+    )  # Predict Y values at test locations
+    plt.plot(
+        x, y, ".", label="training points", alpha=0.2, markersize=ms, color="#264185"
+    )
     if test_data is not None:
         x_test, y_test = test_data
-        plt.plot(x_test, y_test, ".", label="test points", alpha=0.2, markersize='10',color='orange')
-    (line,) = plt.plot(pX, pY, lw=2.5, label="posterior mean", color='#A2D2FF', alpha=1.0)
+        plt.plot(
+            x_test,
+            y_test,
+            ".",
+            label="test points",
+            alpha=0.2,
+            markersize=ms,
+            color="orange",
+        )
+    (line,) = plt.plot(
+        pX, pY, lw=2.5, label="posterior mean", color="#A2D2FF", alpha=1.0
+    )
     plt.fill_between(
         pX[:, 0],
-        (pY - 2 * pYv ** 0.5)[:, 0],
-        (pY + 2 * pYv ** 0.5)[:, 0],
-        color='#A2D2FF',
+        (pY - 2 * pYv**0.5)[:, 0],
+        (pY + 2 * pYv**0.5)[:, 0],
+        color="#A2D2FF",
         alpha=0.4,
         lw=1.5,
     )
     if is_svgp:
         Z = model.inducing_variable.Z.numpy()
-        plt.plot(Z, np.repeat(ylim[0], Z.shape[0]), 'k^', markersize='30', label="inducing locations", alpha=0.5, color='black')
+        plt.plot(
+            Z,
+            np.repeat(ylim[0], Z.shape[0]),
+            "k^",
+            markersize="30",
+            label="inducing locations",
+            alpha=0.5,
+            color="black",
+        )
     plt.legend(loc="lower right")
     plt.ylim(ylim)
     sns.despine()
     plt.tight_layout()
 
 
-def plot_grad_norms(norms:list, max_norm:int, cmap:str='viridis', n_sample=100, vis_mult=100):
+def plot_grad_norms(
+    norms: list, max_norm: int, cmap: str = "viridis", n_sample=100, vis_mult=100
+):
     cmap = mpl.cm.get_cmap(cmap)
     fig = plt.figure(figsize=(5, 10))
     kde = KernelDensity(kernel="tophat", bandwidth=2.0)
@@ -57,10 +93,12 @@ def plot_grad_norms(norms:list, max_norm:int, cmap:str='viridis', n_sample=100, 
             color = cmap(i / len(norms))
             plt.plot(X_plot[:, 0], vis_mult * np.exp(log_dens) + i, color=color)
             plt.fill_between(
-                X_plot[:, 0], vis_mult * np.exp(log_dens) + i, i, color=color, zorder=300 - i
+                X_plot[:, 0],
+                vis_mult * np.exp(log_dens) + i,
+                i,
+                color=color,
+                zorder=300 - i,
             )
 
     plt.xlim((0, max_norm))
     plt.ylim((-1, len(norms) + 50))
-
-